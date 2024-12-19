d3436fbb703409cddb62c3068e4d21bd58f774c1
styven <styvenn16@gmail.com>
2024-12-19 20:43:06 -0300

    Habilitando o CORS

--- app/app.py
+++ app/app.py
@@ -5,10 +5,20 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from models.user_models import User
 from models.task_model import Task
 from api.api_v1.router import router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f'{settings.API_V1_STR}/openapi.json'
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 @app.on_event("startup")
 async def app_init():

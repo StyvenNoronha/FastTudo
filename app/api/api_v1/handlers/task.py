28e1cf5296fd29251964181e4583b2681f85810c
styven <styvenn16@gmail.com>
2024-12-19 19:53:34 -0300

    Criando o Schema

--- app/api/api_v1/handlers/task.py
+++ app/api/api_v1/handlers/task.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from schemas.task_schema import TaskDetail
+from models.user_models import User
+from api.dependencies.user_deps import get_current_user
 
 task_router = APIRouter()
 
-@task_router.get('/test')
-async def teste():
-    return "Tarefas Listadas"
+@task_router.get('/', summary='Lista as Tarefas', response_model=TaskDetail)
+async def list_tasks(user: User = Depends(get_current_user)):
+    pass
